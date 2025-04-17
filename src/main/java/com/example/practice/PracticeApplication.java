e91ffb16b1cdef78ff5769382cd11c9fac4a0de5
ADMIN <nayna7acharya@gmail.com>
2025-04-17 16:55:49 +0530

    db values

--- src/main/java/com/example/practice/PracticeApplication.java
+++ src/main/java/com/example/practice/PracticeApplication.java
@@ -18,7 +18,7 @@ public class PracticeApplication {
 	}
     @GetMapping("/test")
     public String test(){
-        return "inserting new values in db checking";
+        return "inserting new values in db checking the content";
     }
 
 }
