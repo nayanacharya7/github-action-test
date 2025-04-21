f13b554399e4706f7fbcd41100f5a662c2344f20
nayanacharya7 <nayan7acharya@gmail.com>
2025-04-21 16:55:57 +0530

    Update HelloController.java

--- myapp/src/main/java/com/example/controller/HelloController.java
+++ myapp/src/main/java/com/example/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @GetMapping("/")
   public String hello() {
-    return "Hello from Spring Boot with CI/CD! aa aaaaaa ddsa aaa entry hdfdsafdsafdfda dfsvfvdsfda";
+    return "Hello from Spring Boot with CI/CD! aa aaaaaa ddsa aaa entry hdfdsafdsafdfda dfsvfvdsfda ewfewfawweaewasa";
   }
 }
